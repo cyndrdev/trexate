a3da12dd5444ea7c560e55e572bc0cd84d48f758
cyndrdev <me@cyndr.uk>
2019-04-13 22:09:32 +0100

    added framerate counter for debug

---
+++ Assets/Scripts/UI/FramerateCounter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FramerateCounter : MonoBehaviour
+{
+    private Text _counter;
+    private float _framerate;
+    [SerializeField]
+    [Range(0f, 1f)]
+    private float _smoothing;
+
+    void Start()
+    {
+        _counter = this.GetComponent<Text>();
+
+        if (_counter == null)
+            throw new System.Exception();
+    }
+
+    void Update()
+    {
+        float currentFramerate = 1f / Time.deltaTime;
+        _framerate = Mathf.Lerp(_framerate, currentFramerate, _smoothing);
+        _counter.text = ((int)_framerate).ToString();
+    }
+}
